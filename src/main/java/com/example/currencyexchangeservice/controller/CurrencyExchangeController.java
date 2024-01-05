c635f6a360e1d4decb10a1cb5a52053010f5e1ae
Victor Duran <victor.duran.em@gmail.com>
2024-01-05 17:40:48 -0500

    Add telemetry

--- src/main/java/com/example/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/example/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.example.currencyexchangeservice.controller;
 
 import com.example.currencyexchangeservice.model.CurrencyExchange;
 import com.example.currencyexchangeservice.repository.CurrencyExchangeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyExchangeController {
 
+    private final Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
     private final Environment environment;
     private final CurrencyExchangeRepository repository;
 
@@ -23,6 +26,8 @@ public class CurrencyExchangeController {
             @PathVariable String from,
             @PathVariable String to
     ) {
+
+        logger.info("retrieveExchangeValue called with {} to {}", from, to);
         CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
 
         if (currencyExchange == null) {
